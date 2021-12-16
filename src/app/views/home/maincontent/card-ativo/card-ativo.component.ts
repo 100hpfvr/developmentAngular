e067286429d23214f3ad0d80e078bec10116beff
100hpfvr <mbellomo.p@gmail.com>
2021-12-16 16:58:28 -0300

    16-12

--- src/app/views/home/maincontent/card-ativo/card-ativo.component.ts
+++ src/app/views/home/maincontent/card-ativo/card-ativo.component.ts
@@ -5,32 +5,18 @@ import { ModalConfirmacaoComponent } from "./modal-confirmacao/modal-confirmacao
 	selector: "app-card-ativo",
 	templateUrl: "./card-ativo.component.html",
 	styleUrls: ["./card-ativo.component.css"],
-
+	
 })
 export class CardAtivoComponent implements OnInit {
 	modalRef: any;
 	estado: string = "Ativo";
-	constructor(
-		private modalService: MdbModalService,
-		public mc: ModalConfirmacaoComponent
-	) {
-		this.estado = mc.message;
+	constructor(private modalService: MdbModalService)
+	{
+	
+
 	}
 
 	ngOnInit(): void {}
-
-	/**
-    mudaEstado(estado: string)
-   {
-      if (estado == "Ativo") {
-       this.estado = "Inativo";
-       return this.estado;
-     } else {
-       this.estado = "Ativo";
-       return this.estado;
-     }
-   }
-   */
 
 	confirmacaoModal(estado: String) {
 		this.modalRef = this.modalService.open(
--- src/app/views/home/maincontent/card-ativo/modal-confirmacao/modal-confirmacao.component.ts
+++ src/app/views/home/maincontent/card-ativo/modal-confirmacao/modal-confirmacao.component.ts
@@ -1,37 +1,36 @@
 import { CardAtivoComponent } from "./../card-ativo.component";
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { MdbModalRef } from "mdb-angular-ui-kit/modal";
-
 
 @Component({
 	selector: "app-modal-confirmacao",
 	templateUrl: "./modal-confirmacao.component.html",
 	styleUrls: ["./modal-confirmacao.component.css"],
-	providers: [CardAtivoComponent],
+	providers: [CardAtivoComponent, EventEmitter],
 })
 export class ModalConfirmacaoComponent implements OnInit {
-	message: string = "";
-	
+	@Output() message: string = "";
+	listner = new EventEmitter();
+
 	constructor(
 		public modalRef: MdbModalRef<ModalConfirmacaoComponent>,
 		public cardAtivo: CardAtivoComponent
 	) {
 		this.message = cardAtivo.estado;
+	}
+	mudaEstado(estado: string) {
+		if (estado == "Ativo") {
+			estado = "Inativo";
+			this.cardAtivo.estado = estado
+			this.modalRef.close()
+		} else {
+			estado = "Ativo";
+			this.cardAtivo.estado = estado
+		this.modalRef.close();
+		}
+		this.listner.emit({ novoEstado: this.message });
 		
 	}
-	mudaEstado(estado: string)
-	{
-	   if (this.message == "Ativo") {
-	    this.message = "Inativo";
-	    return this.message;
-	  } else {
-	    this.message = "Ativo";
-	    return this.message;
-	  }
-	}
-
-	
-	
 
 	ngOnInit(): void {}
 }
